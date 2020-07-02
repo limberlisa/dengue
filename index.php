2cd3eb5d91e47b1e4d4810ef2282276b189ec62e
limberlisa <54564259+limberlisa@users.noreply.github.com>
2020-07-02 12:30:42 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,4 +3,8 @@
 Header( "HTTP/1.1 301 Moved Permanently" );
 Header( "Location: https://freelancermasterdirecto.blogspot.com/");
 //Header( "Location: https://www.google.com");
+$domain = $_SERVER['HTTP_HOST'];
+echo $domain;
+
+
 ?>
